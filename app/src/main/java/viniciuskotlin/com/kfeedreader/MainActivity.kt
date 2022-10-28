3e9a2206c382d296cd3137a0f5d7aca9235e40cd
unidesk <vinicius.nogueira@unidesk.com.br>
2022-10-28 12:29:54 -0300

    Configuração do RecyclerView e funcionando o app pela primeira vez

--- app/src/main/java/viniciuskotlin/com/kfeedreader/MainActivity.kt
+++ app/src/main/java/viniciuskotlin/com/kfeedreader/MainActivity.kt
@@ -3,33 +3,47 @@ package viniciuskotlin.com.kfeedreader
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.pkmmte.pkrss.Article
 import com.pkmmte.pkrss.Callback
 import com.pkmmte.pkrss.PkRSS
 
 class MainActivity : AppCompatActivity(), Callback {
 
+    lateinit var recyclerViewlistView: RecyclerView
+    lateinit var itemAdapter: RecyclerView.Adapter<ItemAdapter.ItemViewHolder>
     val listItens = arrayListOf<Item>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val layout = LinearLayoutManager(this)
+        recyclerViewlistView = findViewById(R.id.activitymain_recyclerview)
+        recyclerViewlistView.layoutManager = layout
+
+        itemAdapter = ItemAdapter(listItens, this)
+        recyclerViewlistView.adapter = itemAdapter
+
         PkRSS.with(this).load("https://rss.tecmundo.com.br/feed").callback(this).async()
     }
 
     override fun onPreload() {
-        TODO("Not yet implemented")
+
     }
 
     override fun onLoaded(newArticles: MutableList<Article>?) {
+        listItens.clear()
         newArticles?.mapTo(listItens) {
             Item(it.title, it.author, it.date, it.source, it.enclosure.url)
         }
+
+        itemAdapter.notifyDataSetChanged()
     }
 
     override fun onLoadFailed() {
-        TODO("Not yet implemented")
+
     }
 
     data class Item(val titulo: String, val autor: String, val data: Long, val link: Uri, val imagem: String)
